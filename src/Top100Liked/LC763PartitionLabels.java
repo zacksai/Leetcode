bccc6fb874d10d3d2be3c813cb5a498b88291acd
Zack Sai <zackjsai@gmail.com>
2022-02-27 16:05:34 -0800

    updated javadocs and variable names

--- src/Top100Liked/LC763PartitionLabels.java
+++ src/Top100Liked/LC763PartitionLabels.java
@@ -4,47 +4,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * ababcbacadefegdehijhklij
- * ab  c  ca
+ * class to solve #763 Partition Labels:
+ *
+ * You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+ *
+ * Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+ *
+ * Return a list of integers representing the size of these parts.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "ababcbacadefegdehijhklij"
+ * Output: [9,7,8]
+ * Explanation:
+ * The partition is "ababcbaca", "defegde", "hijhklij".
+ * This is a partition so that each letter appears in at most one part.
+ * A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+ * Example 2:
+ *
+ * Input: s = "eccbbbbdec"
+ * Output: [10]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 500
+ * s consists of lowercase English letters.
  */
 public class LC763PartitionLabels {
 
     // Start at int i = position 0
     // Find the last occurrence of the letter at i and store that position as p
-    // For every letter between i and p, find its last occurence
+    // For every letter between i and p, find its last occurrence
     // If the last occurrence is after p, update p to the new value
     // Store the difference between i and p in the list
 
     // Then, update i to the position after p and repeat until p = length-1
     // Return count
 
+    /**
+     * solution method
+     * @param s string to partition
+     * @return list of integer sizes of each partition
+     */
     public List<Integer> partitionLabels(String s) {
 
-        // i for starting position, p for ending position
+        // i for starting position, p for ending position, c for current character, r for returnList
         int i = 0, p = 0;
-        char currentChar = '@';
+        char c = '@';
         List<Integer> r = new ArrayList<Integer>();
 
+        // Iterate through each substring
         for (int j = 0; j < s.length(); j++) {
 
-            // Find the last occurrence of the letter at i and store that position as p
-
             // Initialize c to the value at i and p to its index
-            currentChar = s.charAt(i);
-            p = s.lastIndexOf(currentChar);
+            c = s.charAt(i);
+            p = s.lastIndexOf(c);
 
-
-            for (int substringPosition = i + 1; substringPosition < p; substringPosition++) {
+            // Iterate through each unique value in the substring
+            for (int k = i + 1; k < p; k++) {
 
                 // First, find the next unique character and load it into c
-                if (s.charAt(substringPosition) == currentChar) continue;
-                else currentChar = s.charAt(substringPosition);
+                if (s.charAt(k) == c) continue;
+                else c = s.charAt(k);
 
                 // Next, find the last index of this value. If it's > p, update p to it
-                if (s.lastIndexOf(currentChar) > p) {
-                    p = s.lastIndexOf(currentChar);
+                if (s.lastIndexOf(c) > p) {
+                    p = s.lastIndexOf(c);
                 }
-                
                 // Repeat for each unique value in the substring
             }
             
